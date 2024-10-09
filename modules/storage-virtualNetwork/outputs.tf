73a2b1b09f5616809e134d6d1032ff307ef2a5e4
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-10-09 12:36:53 +0200

    modified virtual network

--- modules/storage-virtualNetwork/outputs.tf
+++ modules/storage-virtualNetwork/outputs.tf
@@ -22,10 +22,3 @@ output "private_subnet_id" {
   value = azurerm_subnet.private.id
 }
 
-output "public_subnet_id" {
-  value = azurerm_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = azurerm_subnet.private.id
-}
